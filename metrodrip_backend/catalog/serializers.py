49525186daa876f4456f1604e4a87f27a3e2ca0f
JhimmyFiggins <146190916+JhimmyFiggins@users.noreply.github.com>
2026-09-14 15:27:56 +0800

    Update serializers.py

--- metrodrip_backend/catalog/serializers.py
+++ metrodrip_backend/catalog/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import CatalogProduct, CatalogCategory, CatalogProductVariant, InventoryStockEntry,
+from .models import CatalogProduct, CatalogCategory, CatalogProductVariant, InventoryStockEntry
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
